a145aa725237cc7529141fde01050b9c6cde11d0
Itai Valach <63971280+ItaiUukl@users.noreply.github.com>
2024-08-06 23:54:18 +0300

    Fixed font, added movies choice, changed to click anywhere

--- static/js/app.js
+++ static/js/app.js
@@ -12,42 +12,20 @@ const playMovie = function (name) {
   videoNode.src = moviesDict[name]["url"];
 };
 
-
-const localFileVideoPlayer = async () => {
-    const moviePicked = function (event) {
-        playMovie(this.value);
-    };
-
-    await fetchMovies()
-
-    const moviePicker = document.querySelector('select.picker')
-    for (let key in moviesDict) {
-        let opt = document.createElement('option');
-        opt.value = key;
-        opt.innerHTML = key;
-        moviePicker.appendChild(opt);
-    }
-    moviePicker.addEventListener('change', moviePicked, false)
-
-};
-
-localFileVideoPlayer().then()
-
 document.addEventListener('DOMContentLoaded', () => {
-  const toggleButton = document.getElementById('toggleButton');
   const myList = document.getElementById('lista');
   const listItems = myList.querySelectorAll('li');
 
-  toggleButton.addEventListener('click', () => { // function to show/hide menu
+
+  const showMenu = function () { // function to show/hide menu
       if (myList.style.display === 'none' || myList.style.display === '') {
           myList.style.display = 'block';
-          toggleButton.textContent = 'Hide List';
       } else {
           myList.style.display = 'none';
-          toggleButton.textContent = 'Show List';
       }
-//        myList.classList.toggle('show');
-  });
+  }
+  document.body.addEventListener('click', showMenu);
+
   myList.addEventListener('mouseover', (event) => {
             if (event.target.tagName === 'LI' || event.target.closest('li')) {
                 const li = event.target.tagName === 'LI' ? event.target : event.target.closest('li');
@@ -55,43 +33,45 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.body.style.backgroundColor = mainText;
                     }
         });
-
-  listItems.forEach(item => {
-    item.addEventListener('click', (event) => {
-        const clickedItem = event.target; // Get the clicked list item
-        console.log(`You clicked on ${clickedItem.textContent}`);
-        // Call your function here
-        handleListItemClick(clickedItem.textContent);
-    });
+  myList.addEventListener('click', (event) => {
+      event.stopPropagation();
   });
 
-  function handleListItemClick(itemText) {
-    myList.style.display = 'none';
-    toggleButton.textContent = 'Show List';
-    var l=document.getElementById("1")
-    l.innerHTML=itemText
+  const playMovieOnClick = function (event) {
+      myList.style.display = 'none';
+
+      const itemText = event.target.textContent;
+      playMovie(itemText);
   }
 
+  listItems.forEach(item => {item.addEventListener('click', playMovieOnClick)});
+
   function addListItem(mainText, secondaryText) {
-          const li = document.createElement('li');
-          const a = document.createElement('a');
-          const mainSpan = document.createElement('span');
-          const secondarySpan = document.createElement('span');
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      const mainSpan = document.createElement('span');
+      const secondarySpan = document.createElement('span');
 
-          mainSpan.classList.add('main-text');
-          secondarySpan.classList.add('secondary-text');
+      mainSpan.classList.add('main-text');
+      secondarySpan.classList.add('secondary-text');
 
-          mainSpan.textContent = mainText;
-          secondarySpan.textContent = secondaryText;
+      mainSpan.textContent = mainText;
+      secondarySpan.textContent = secondaryText;
 
-          a.appendChild(mainSpan);
-          a.appendChild(secondarySpan);
-          li.appendChild(a);
-          myList.appendChild(li);
+      a.appendChild(mainSpan);
+      a.appendChild(secondarySpan);
+      li.appendChild(a);
+
+      li.addEventListener('click', playMovieOnClick)
+
+      myList.appendChild(li);
+  }
+
+  fetchMovies().then(() => {
+      for(let name in moviesDict) {
+          addListItem(name, "author")
       }
-
-
-
+  })
 
 });
 
