4b7a01425fa80f9cb56d90adb54d56b011633847
Itai Valach <63971280+ItaiUukl@users.noreply.github.com>
2024-08-07 08:19:02 +0300

    Movies controlled by code

--- static/js/app.js
+++ static/js/app.js
@@ -1,79 +1,94 @@
 let moviesDict = {};
+let playedMovie = '';
 
 
 const fetchMovies = async () => {
-  const res = await fetch('/movies')
-  moviesDict = await res.json()
+    const res = await fetch('/movies')
+    moviesDict = await res.json()
+    for (let name in moviesDict) {
+        moviesDict[name]["file"] = await fetch('/movie/' + name);
+    }
 };
 
 const playMovie = function (name) {
-  const videoNode = document.querySelector('video')
+    const videoNode = document.querySelector('video')
 
-  videoNode.src = moviesDict[name]["url"];
+    videoNode.style.display = 'block';
+    if (name !== playedMovie) {
+        videoNode.src = moviesDict[name]["url"];
+        playedMovie = name;
+    }
+    videoNode.play().then()
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  const myList = document.getElementById('lista');
-  const listItems = myList.querySelectorAll('li');
+    const myList = document.getElementById('lista');
+    const listItems = myList.querySelectorAll('li');
+
+    const videoNode = document.querySelector('video')
 
 
-  const showMenu = function () { // function to show/hide menu
-      if (myList.style.display === 'none' || myList.style.display === '') {
-          myList.style.display = 'block';
-      } else {
-          myList.style.display = 'none';
-      }
-  }
-  document.body.addEventListener('click', showMenu);
+    videoNode.style.display = 'none';
+    videoNode.addEventListener("click", event => event.preventDefault())
 
-  myList.addEventListener('mouseover', (event) => {
-            if (event.target.tagName === 'LI' || event.target.closest('li')) {
-                const li = event.target.tagName === 'LI' ? event.target : event.target.closest('li');
-                const mainText = li.querySelector('.main-text').textContent;
-                document.body.style.backgroundColor = mainText;
-                    }
-        });
-  myList.addEventListener('click', (event) => {
-      event.stopPropagation();
-  });
+    const showMenu = function () { // function to show/hide menu
+        if (myList.style.display === 'none' || myList.style.display === '') {
+            myList.style.display = 'block';
+            videoNode.style.display = 'none';
+            videoNode.pause().then()
+        } else {
+            myList.style.display = 'none';
+        }
+    }
+    document.body.addEventListener('click', showMenu);
 
-  const playMovieOnClick = function (event) {
-      myList.style.display = 'none';
+    myList.addEventListener('mouseover', (event) => {
+        if (event.target.tagName === 'LI' || event.target.closest('li')) {
+            const li = event.target.tagName === 'LI' ? event.target : event.target.closest('li');
+            const mainText = li.querySelector('.main-text').textContent;
+            document.body.style.backgroundColor = mainText;
+        }
+    });
+    myList.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
 
-      const itemText = event.target.textContent;
-      playMovie(itemText);
-  }
+    const playMovieOnClick = function (event) {
+        myList.style.display = 'none';
 
-  listItems.forEach(item => {item.addEventListener('click', playMovieOnClick)});
+        const itemText = event.target.textContent;
+        playMovie(itemText);
+    }
 
-  function addListItem(mainText, secondaryText) {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      const mainSpan = document.createElement('span');
-      const secondarySpan = document.createElement('span');
+    listItems.forEach(item => {
+        item.addEventListener('click', playMovieOnClick)
+    });
 
-      mainSpan.classList.add('main-text');
-      secondarySpan.classList.add('secondary-text');
+    function addListItem(mainText, secondaryText) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const mainSpan = document.createElement('span');
+        const secondarySpan = document.createElement('span');
 
-      mainSpan.textContent = mainText;
-      secondarySpan.textContent = secondaryText;
+        mainSpan.classList.add('main-text');
+        secondarySpan.classList.add('secondary-text');
 
-      a.appendChild(mainSpan);
-      a.appendChild(secondarySpan);
-      li.appendChild(a);
+        mainSpan.textContent = mainText;
+        secondarySpan.textContent = secondaryText;
 
-      li.addEventListener('click', playMovieOnClick)
+        a.appendChild(mainSpan);
+        a.appendChild(secondarySpan);
+        li.appendChild(a);
 
-      myList.appendChild(li);
-  }
+        li.addEventListener('click', playMovieOnClick)
 
-  fetchMovies().then(() => {
-      for(let name in moviesDict) {
-          addListItem(name, "author")
-      }
-  })
+        myList.appendChild(li);
+    }
+
+    fetchMovies().then(() => {
+        for (let name in moviesDict) {
+            addListItem(name, "author")
+        }
+    })
 
 });
-
-
-
